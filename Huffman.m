5eb90eacbd2f90d0919d1c28a09d11ccff983961
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-08-15 20:29:19 -0400

    Update Huffman.m

--- Huffman.m
+++ Huffman.m
@@ -3,8 +3,20 @@ clear, clc
 P = [0.30, 0.25, 0.15, 0.12, 0.08, 0.10];
 P = sort(P,'descend');
 
+p = [];
 while numel(P) > 2
     P(end-1) = P(end-1) + P(end);
     P(end) = [];
-    P = sort(P,'descend')
+    [P,I] = sort(P,'descend');
+    [~,I] = max(I);
+    p(end+1) = I;
 end
+
+code = {'0','1'};
+for i = flip(p)
+    code(end+1) = strcat(code(i),'0');
+    code(end+1) = strcat(code(i),'1');
+    code(i) = [];
+end
+
+code'
