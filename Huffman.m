8248858e28a63819277bd1bf7b9c8415a6205093
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-08-15 23:04:16 -0400

    Update Huffman.m

--- Huffman.m
+++ Huffman.m
@@ -18,3 +18,14 @@ for i = flip(p)
 end
 
 code
+
+P = [0.30, 0.25, 0.15, 0.12, 0.08, 0.10];
+P = sort(P,'descend');
+
+L = sum(P.*cellfun('length',code))
+
+H = sum(P.*log2(1./P))
+
+eta = H/L
+
+gama = 1 - eta
