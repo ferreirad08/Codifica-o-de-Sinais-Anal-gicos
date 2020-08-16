cfd2fc406dec7673d98dad838975faa190536d3e
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-08-16 00:59:06 -0400

    Update Huffman.m

--- Huffman.m
+++ Huffman.m
@@ -1,13 +1,13 @@
 P = [0.30, 0.25, 0.15, 0.12, 0.08, 0.10];
 P = sort(P,'descend')
 
-p = [];
+h = [];
 while numel(P) > 2
     P(end-1) = P(end-1) + P(end);
     P(end) = [];
     [P,I] = sort(P,'descend');
     [~,I] = max(I);
-    p(end+1) = I;
+    h(end+1) = I;                  % Historic
 end
 
 code = {'0','1'};
