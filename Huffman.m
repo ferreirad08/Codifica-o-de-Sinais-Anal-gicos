d740f5bb3cb5bd6932a3f62ca07837344a2372b2
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-08-15 23:37:07 -0400

    Update Huffman.m

--- Huffman.m
+++ Huffman.m
@@ -1,4 +1,7 @@
-P = [0.30, 0.25, 0.15, 0.12, 0.08, 0.10];
+P = [0.30, 0.25, 0.15, 0.12, 0.08, 0.10]
+
+H = sum(P.*log2(1./P))             % Entropy
+
 P = sort(P,'descend');
 
 p = [];
@@ -23,6 +26,5 @@ P = [0.30, 0.25, 0.15, 0.12, 0.08, 0.10];
 P = sort(P,'descend');
 
 L = sum(P.*cellfun('length',code)) % Length
-H = sum(P.*log2(1./P))             % Entropy
 eta = H/L                          % Efficiency
 gama = 1 - eta                     % Redundancy
