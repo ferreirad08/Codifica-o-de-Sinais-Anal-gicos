a822e3b541903d75b999149c8d639b5e8406dc7f
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-09-24 13:12:36 -0400

    Create arithmetic_coding.m

---
+++ arithmetic_coding.m
@@ -0,0 +1,31 @@
+P = [0.6 0.4]; % probabilities
+S = [1 1 1]; % sequence of symbols
+
+A = [0 cumsum(P)]; % zero and accumulated probabilities
+intervals = A; % first intervals
+for i = S % for each symbol
+    low = intervals(i);
+    high = intervals(i+1);
+    middle = low+(high-low).*A(2:end-1); % new limits
+    intervals = [low middle high]; % new intervals
+end
+
+interval = intervals([1 end]) % final interval
+
+y = 100;
+for number = interval(1)+.00001:0.00001:interval(2)
+    binary = [];
+    while number > 0
+        number = number*2;
+        binary(end+1) = floor(number); % integer part
+        number = number-floor(number); % decimal part
+    end
+    
+    L = length(binary); % code length
+    if L < y
+        binStr = num2str(binary); % code in string
+        y = L;
+    end
+end
+
+binStr % final code
