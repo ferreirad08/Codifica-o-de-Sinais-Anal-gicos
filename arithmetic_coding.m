c82e0e2802a141a960b2802ac99f7c16592aa36b
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-09-24 23:27:02 -0400

    Update arithmetic_coding.m

--- arithmetic_coding.m
+++ arithmetic_coding.m
@@ -1,6 +1,14 @@
 P = [0.6 0.3 0.1]; % probabilities
 S = [1 2 1]; % sequence of symbols
 
+P = [0.5, 0.1, 0.2, 0.1, 0.1]; % probabilities
+M = {'S', 'W', 'I', 'M', '_'}; % respective symbols
+S = {'S', 'W', 'I', 'S', 'S', '_', 'M', 'I', 'S', 'S'}; % sequence of symbols
+
+[P,I] = sort(P,'descend');
+M = M(I);
+[~,S] = ismember(S,M);
+
 A = [0 cumsum(P)]; % zero and accumulated probabilities
 intervals = A; % first intervals
 for i = S % for each symbol
