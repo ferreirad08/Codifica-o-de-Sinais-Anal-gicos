f927297442227c6b78601b64d822bd70183e14d5
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-09-29 13:20:33 -0400

    Update shannon_fano_coding.m

--- shannon_fano_coding.m
+++ shannon_fano_coding.m
@@ -2,7 +2,7 @@ P = [0.5 0.25 0.125 0.125]
 
 diff = []
 for i = 1:numel(P)-1
-    diff = [diff sum(P(1:i))-sum(P(i+1:end))];
+    diff(end+1) = sum(P(1:i))-sum(P(i+1:end));
 end
 
 [~,i] = min(diff);
