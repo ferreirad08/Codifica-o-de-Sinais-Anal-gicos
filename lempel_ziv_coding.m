fd1b79d3930a2991fc257134871c36e483ac7938
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-09-29 11:23:49 -0400

    Update lempel_ziv_coding.m

--- lempel_ziv_coding.m
+++ lempel_ziv_coding.m
@@ -1,11 +1,11 @@
 S = {'a','a','a','b','a','b','b','b','a','a','a','b','a','a','a','a','a','a','a','b','a','a','b','b'};
 
 dict = S(1);
-for i = S(2:end)
+for item = S(2:end)
     if ismember(dict(end),dict(1:end-1))
-        dict(end) = strcat(dict(end),i);
+        dict(end) = strcat(dict(end),item);
     else
-        dict(end+1) = i;
+        dict(end+1) = item;
     end
 end
 
