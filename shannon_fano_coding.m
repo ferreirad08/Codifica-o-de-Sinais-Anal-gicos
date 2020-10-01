20ad68735be85b0a786609c4c2557fc0714734cb
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-01 13:07:11 -0400

    Update shannon_fano_coding.m

--- shannon_fano_coding.m
+++ shannon_fano_coding.m
@@ -1,13 +1,18 @@
-P = [1/2 1/4 1/8 1/16 1/32 1/64 1/64]
-C = {'','','','','','',''};
+% Author: Prof. M.Sc. David Ferreira - Federal University of Amazonas
+% Contact: ferreirad08@gmail.com
 
-while numel(unique(C)) < numel(P)
-    for i = unique(C)
-        idx = find(strcmp(C,i));
+P = [1/2 1/4 1/8 1/16 1/32 1/64 1/64]; % probabilities
+
+Psort = sort(P,'descend') % ordered probabilities
+code = {'','','','','','',''};
+
+while numel(unique(code)) < numel(Psort)
+    for i = unique(code)
+        idx = find(strcmp(code,i));
         n = numel(idx);
         if n > 1
             diff = [];
-            Pt = P(idx);
+            Pt = Psort(idx);
             for i = 1:n-1
                 diff(end+1) = abs(sum(Pt(1:i))...
                     -sum(Pt(i+1:end)));
@@ -16,14 +21,19 @@ while numel(unique(C)) < numel(P)
             [~,i] = min(diff);
         
             for j = idx(1:i)
-                C{j} = strcat(C{j},'0');
+                code{j} = strcat(code{j},'0');
             end
 
             for j = idx(i+1:n)
-                C{j} = strcat(C{j},'1');
+                code{j} = strcat(code{j},'1');
             end
         end
     end
 end
 
-C
+code % final code
+
+H = -sum(P.*log2(P)) % entropy
+L = sum(Psort.*cellfun('length',code)) % length
+eta = H/L % efficiency
+gama = 1-eta % redundancy
