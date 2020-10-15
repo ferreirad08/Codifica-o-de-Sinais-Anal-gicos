1fe7a2f75b0995804dc4c7ebe47031cb03719953
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-15 09:01:42 -0400

    Update mutual_information.m

--- mutual_information.m
+++ mutual_information.m
@@ -9,12 +9,12 @@ Hx = -sum(Px.*log2(Px)) % entropy of the transmitter (source)
 
 [m,n] = size(Pyx) % number of inputs and outputs
 
-Py = sum(Pyx.*repmat(Px',1,2))
-Pxy = (Pyx.*repmat(Px',1,2))./repmat(Py,2,1)
+Py = sum(Pyx.*repmat(Px',1,n))
+Pxy = (Pyx.*repmat(Px',1,n))./repmat(Py,m,1)
 
 Hy = -sum(Py.*log2(Py)) % entropy of the receiver
 
-Hxy = sum(sum(repmat(Py,2,1).*Pxy.*log2(1./Pxy))) % conditional entropy of x given y
+Hxy = sum(sum(repmat(Py,m,1).*Pxy.*log2(1./Pxy))) % conditional entropy of x given y
 
 Ixy = Hx - Hxy % mutual information of x and y
 
