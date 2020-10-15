aedac339b132b55661f70e3a6ac2b5e25583428c
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-14 23:42:36 -0400

    Update mutual_information.m

--- mutual_information.m
+++ mutual_information.m
@@ -8,13 +8,12 @@ Pyx = [2/3 1/3; 1/10 9/10] % matriz de canal
 Hx = -sum(Px.*log2(Px)) % entropia de x
 
 Py = sum(Pyx.*repmat(Px',1,2))
-
 Pxy = (Pyx.*repmat(Px',1,2))./repmat(Py,2,1) % probabilidades condicionais a posteriori
+
+Hy = -sum(Py.*log2(Py)) % entropia de y
 
 Hxy = repmat(Py,2,1).*Pxy.*log2(1./Pxy);
 Hxy = sum(Hxy(:)) % entropia condicional de x dado y
-
-Hy = -sum(Py.*log2(Py)) % entropia de y
 
 Ixy = Hx - Hxy % informação mútua de x e y
 
