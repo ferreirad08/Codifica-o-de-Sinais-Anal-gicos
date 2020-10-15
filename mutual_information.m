e28228dc2a6619ba29cca22681926d15bb292a46
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-15 00:12:13 -0400

    Update mutual_information.m

--- mutual_information.m
+++ mutual_information.m
@@ -3,17 +3,17 @@
 % Date: October 2020
 
 Px = [1/3 2/3] % probabilidades dos símbolos da fonte
-Pyx = [2/3 1/3; 1/10 9/10] % matriz de canal
+Pyx = [2/3 1/3; 1/10 9/10] % channel matrix
 
-Hx = -sum(Px.*log2(Px)) % entropia de x
+Hx = -sum(Px.*log2(Px)) % entropy of source
 
 Py = sum(Pyx.*repmat(Px',1,2))
 Pxy = (Pyx.*repmat(Px',1,2))./repmat(Py,2,1) % probabilidades condicionais a posteriori
 
-Hy = -sum(Py.*log2(Py)) % entropia de y
+Hy = -sum(Py.*log2(Py)) % entropy of y
 
-Hxy = sum(sum(repmat(Py,2,1).*Pxy.*log2(1./Pxy))) % entropia condicional de x dado y
+Hxy = sum(sum(repmat(Py,2,1).*Pxy.*log2(1./Pxy))) % conditional entropy of x given y
 
-Ixy = Hx - Hxy % informação mútua de x e y
+Ixy = Hx - Hxy % mutual information of x and y
 
-Hyx = Hy - Ixy % entropia condicional de y dado x
+Hyx = Hy - Ixy % conditional entropy of y given x
