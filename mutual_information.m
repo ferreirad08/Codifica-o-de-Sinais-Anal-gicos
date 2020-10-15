665ff383c25bde19373e034e1c02e74e385931d6
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-15 00:20:50 -0400

    Update mutual_information.m

--- mutual_information.m
+++ mutual_information.m
@@ -2,7 +2,7 @@
 % Contact: ferreirad08@gmail.com
 % Date: October 2020
 
-Px = [1/3 2/3] % probabilidades dos símbolos da fonte
+Px = [1/3 2/3] % symbol probabilities
 Pyx = [2/3 1/3; 1/10 9/10] % channel matrix
 
 Hx = -sum(Px.*log2(Px)) % entropy of the source
