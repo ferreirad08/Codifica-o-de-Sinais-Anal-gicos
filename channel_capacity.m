de5f7a7097ad3bc6185e673ab097a193b9657da2
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-18 14:53:02 -0400

    Update channel_capacity.m

--- channel_capacity.m
+++ channel_capacity.m
@@ -4,20 +4,15 @@
 
 Pyx = [1 0 0; 0 1 0; 1/2 1/2 0] % channel transition matrix
 
-Hx = -sum(Px.*log2(Px)) % entropy of the transmitter (source)
+Hx = log2(m) % entropy of the transmitter (source) with uniform distribution
 
 [m,n] = size(Pyx) % number of inputs and outputs
 
 Py = sum(Pyx.*repmat(Px',1,n))
 Pxy = (Pyx.*repmat(Px',1,n))./repmat(Py,m,1)
 
-Hy = -sum(Py.*log2(Py),'omitnan') % entropy of the receiver
-
 Hxy = sum(sum(repmat(Py,m,1).*Pxy.*log2(1./Pxy),'omitnan')) % conditional entropy of x given y
 
-Ixy = Hx - Hxy % mutual information of x and y
-
-Hx = log2(m) % symbol/message probabilities with uniform distribution
 C = Hx - Hxy % channel capacity
 
 Hrow = -sum(Px.*log2(Px))
