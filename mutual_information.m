7651ed041c715e2109c5fcb6a571122ea9d851f1
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-19 10:29:12 -0400

    Update mutual_information.m

--- mutual_information.m
+++ mutual_information.m
@@ -3,17 +3,17 @@
 % Date: October 2020
 
 Px = [1/4 1/4 1/2] % symbol/message probabilities
-Pyx = [1 0 0; 0 1 0; 1/2 1/2 0] % channel transition matrix
+Pzx = [1 0 0; 1 0 0; 0 1 0] % channel transition matrix
 
 Hx = -sum(Px.*log2(Px)) % entropy of the transmitter (source)
 
-[m,n] = size(Pyx) % number of inputs and outputs
+[m,n] = size(Pzx) % number of inputs and outputs
 
-Py = sum(Pyx.*repmat(Px',1,n))
-Pxy = (Pyx.*repmat(Px',1,n))./repmat(Py,m,1)
+Pz = sum(Pzx.*repmat(Px',1,n))
+Pxz = (Pzx.*repmat(Px',1,n))./repmat(Pz,m,1)
 
-Hy = -sum(Py.*log2(Py),'omitnan') % entropy of the receiver
+Hz = -sum(Pz.*log2(Pz),'omitnan') % entropy of the receiver
 
-Hxy = sum(sum(repmat(Py,m,1).*Pxy.*log2(1./Pxy),'omitnan')) % conditional entropy of x given y
+Hxz = sum(sum(repmat(Pz,m,1).*Pxz.*log2(1./Pxz),'omitnan')) % conditional entropy of x given z
 
-Ixy = Hx - Hxy % mutual information of x and y
+Ixz = Hx - Hxz % mutual information of x and z
