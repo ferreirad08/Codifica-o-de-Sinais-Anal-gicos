814facce467d10fb959a8ff0352fe42d21850f78
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-19 11:11:56 -0400

    Update channel_capacity.m

--- channel_capacity.m
+++ channel_capacity.m
@@ -18,7 +18,3 @@ Pxy = (Pyx.*repmat(Px',1,n))./repmat(Py,m,1)
 Hxy = sum(sum(repmat(Py,m,1).*Pxy.*log2(1./Pxy),'omitnan')) % conditional entropy of x given y
 
 C = Hx - Hxy % channel capacity
-
-Hrow = -sum(Pyx(1,:).*log2(Pyx(1,:))) % arbitrary row of the channel transition matrix
-C = log2(n) - Hrow % symmetric channel capacity
-
