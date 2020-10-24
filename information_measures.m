d5e4a50f58db5e9e5ee67705e791ddf729b7decb
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-24 00:29:06 -0400

    Update information_measures.m

--- information_measures.m
+++ information_measures.m
@@ -2,6 +2,8 @@ S = {'A', 'R', 'A', 'R', 'A', 'S'}; % sequence of symbols
 M = {'A', 'R', 'S'}; % symbols
 P = [3/6, 2/6, 1/6]; % respective probabilities
 
+n = ceil(log2(numel(P))) % number of bits per symbol
+
 code = {'00', '01', '10'}; % final code
 
 H = -sum(P.*log2(P)) % entropy
