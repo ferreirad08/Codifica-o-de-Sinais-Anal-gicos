f924bf238fa97ef9ea6d1da8fbfef18398b6a66e
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2020-10-24 00:36:30 -0400

    Update information_measures.m

--- information_measures.m
+++ information_measures.m
@@ -2,8 +2,8 @@ S = {'A', 'R', 'A', 'R', 'A', 'S'}; % sequence of symbols
 M = {'A', 'R', 'S'}; % symbols
 P = [3/6, 2/6, 1/6]; % respective probabilities
 
-n_symb = numel(M) % number of symbols
-n_bits = ceil(log2(n_sym)) % number of bits per symbol
+n_symbols = numel(M) % number of symbols
+n_bits = ceil(log2(n_symbols)) % number of bits per symbol
 
 code = {'00', '01', '10'}; % final code
 
